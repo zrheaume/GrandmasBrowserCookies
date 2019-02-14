d7a98ade945aef77e3de37c1aef2a3cdd49525ef
zrheaume <zachary.a.rheaume@gmail.com>
2019-02-14 18:43:22 -0500

    Exposed build

---
+++ precache-manifest.a17ad072a46e0307861aa59e44b940cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1a32a075989ddfc29d2",
+    "url": "/GrandmasBrowserCookies/static/css/main.d1787b1f.chunk.css"
+  },
+  {
+    "revision": "e1a32a075989ddfc29d2",
+    "url": "/GrandmasBrowserCookies/static/js/main.e1a32a07.chunk.js"
+  },
+  {
+    "revision": "64008741e5819c6b56fb",
+    "url": "/GrandmasBrowserCookies/static/js/1.64008741.chunk.js"
+  },
+  {
+    "revision": "a0a1aaa63c8b4235dba1",
+    "url": "/GrandmasBrowserCookies/static/js/runtime~main.a0a1aaa6.js"
+  },
+  {
+    "revision": "70712d0007bd6ff46759483a7d7d6c5e",
+    "url": "/GrandmasBrowserCookies/index.html"
+  }
+];
